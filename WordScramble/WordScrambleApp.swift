1e4c21ff72dc109dfd05cb2341cb43fbcd47a9f1
dirtcraze <kamil.porebski@icloud.com>
2024-09-26 21:26:33 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kamil Porębski on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
